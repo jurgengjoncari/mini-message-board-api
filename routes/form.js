49c1a9e22b9fb1ee073a79009b658fa1afa516b3
Jurgen n <jurgen.gjoncari@gmail.com>
2021-10-07 22:23:36 +0200

    Remove the unnecessary form route

--- routes/form.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/new', function (req, res, next) {
-    res.render('form', {title: "Form"})
-})
-
-module.exports = router;
