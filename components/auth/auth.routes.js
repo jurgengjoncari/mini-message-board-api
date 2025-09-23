5d8cf1350dfc02d4dc789c10f5fbff1488b52860
jurgengjoncari <j.gjoncari@albansi.com>
2025-09-23 10:34:05 +0200

    remove log for railway

--- components/auth/auth.routes.js
+++ components/auth/auth.routes.js
@@ -36,7 +36,6 @@ authRouter.get('/me', (req, res) => {
 authRouter.post('/logout', (req, res, next) => {
   req.logout(function(err) {
     if (err) { return next(err); }
-    console.log(res);
   });
 });
 
